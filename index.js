413a5fb7183e44e1060dd807f564cbf7abfa92c2
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-04 21:25:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function iterator (arr, task, start=0) {
+    arr.slice(start).forEach( item => {
+        task(item)
+    })
+}
+
+function map (arr, f) {
+    let newArr = []
+    iterator(arr, item => {return newArr.push(f(item))} )
+    return newArr
+}
+
+function reduce (arr, f, start) {
+    let total
+    if (start) {
+        total = start
+        iterator(arr, item => {return total = f(item, total)} )
+    } else {
+        total = arr[0]
+        iterator(arr, item => {return total = f(item, total)}, 1)
+    }
+    return total
+}
+
+
+// iterator(arr, f, [], () => {temp.push(f(item))} )
